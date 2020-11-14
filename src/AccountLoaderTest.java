23c1fc90f1317c0cf92be6f05576c8fec9e45950
zhiquan114514 <69903223+zhiquan114514@users.noreply.github.com>
2020-11-15 05:12:40 +0800

    added AccountLoaderTest class

---
+++ src/AccountLoaderTest.java
@@ -0,0 +1,39 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+public class AccountLoaderTest {
+    private Accounts accounts = Accounts.getInstance();
+    private ArrayList<Account> accountList = accounts.getUser();
+    @BeforeEach
+    public void setup(){
+            accountList.clear();
+            accountList.add(new Account("van","van12345",false,true));
+            accountList.add(new Account("billy","billy12345",true,false));
+           AccountWriter.saveUser();
+    }
+    @AfterEach
+    public void tearDown(){
+        Accounts.getInstance().getUser().clear();
+        AccountWriter.saveUser();
+    }
+    @Test
+    void testGetAccountsSize(){
+        accountList = AccountLoader.loadUsers();
+        assertEquals(2,accountList.size());
+    }
+    @Test
+    void testGetAccountsSizeZero(){
+        Accounts.getInstance().getUser().clear();
+        AccountWriter.saveUser();
+        assertEquals(0,accountList.size());
+    }
+    @Test
+    void testGetAccountFirstUserName(){
+        accountList = AccountLoader.loadUsers();
+        assertEquals("van",accountList.get(0).getName());
+    }
+}
