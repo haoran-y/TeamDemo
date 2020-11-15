ddc67d3ae38ad89c1e213960715aa397d89cbc8e
zhiquan114514 <69903223+zhiquan114514@users.noreply.github.com>
2020-11-16 01:48:24 +0800

    added ListingsTest class

---
+++ src/ListingsTest.java
@@ -0,0 +1,32 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class ListingsTest {
+    Listings listings = Listings.getInstance();
+    private ArrayList<Listing> listingList  = listings.getListingList();
+    @BeforeEach
+    public void setUp(){
+        listingList.clear();
+        listingList.add(new Listing("LiveSC","114514 random","29201",2,
+                2,4,1145.14));
+        listingList.add(new Listing("LiveSC","114514 random","29201",2,
+                1,2,1919.81));
+        ListingWriter.saveListing();
+    }
+    @AfterEach
+    public void tearDown(){
+        listings.getInstance().getListingList() .clear();
+        ListingWriter.saveListing();
+    }
+    @Test
+    void TestSearch(){
+        int[] filterSetting = {2,2,-1,-1,-1,-1,-1,-1,-1};
+        ArrayList<Listing> match = listings.search(filterSetting);
+        assertEquals(listingList.get(0),match.get(0));
+    }
+}
